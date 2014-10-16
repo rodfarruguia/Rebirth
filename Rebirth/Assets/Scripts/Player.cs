d82182ae52e65578ec7ff58209197792015ee3c5
Joe <frizzlefry22@gmail.com>
2014-10-15 22:36:31 -0700

    Basic Movement for Player Sandbox.unity cube that has PlayerScript attached until an Animator is ready.

--- Rebirth/Assets/Scripts/Player.cs
+++ Rebirth/Assets/Scripts/Player.cs
@@ -1,15 +1,30 @@
 ﻿using UnityEngine;
-using System.Collections;
 
-public class Player : MonoBehaviour {
+namespace Assets.Scripts
+{
+    public class Player : MonoBehaviour
+    {
+        public float PlayerMovementSpeed;
 
-	// Use this for initialization
-	void Start () {
+        private Animator _anim;
+        private CharacterController _cont;
+
+        // Use this for initialization
+        void Start ()
+        {
+            _anim = GetComponent<Animator>();
+            _cont = GetComponent<CharacterController>();
+        }
 	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+        // Update is called once per frame
+        void Update () {
+
+            var deltaX = Input.GetAxis( "Horizontal" ) * PlayerMovementSpeed;
+            var deltaY = Input.GetAxis( "Vertical" ) * PlayerMovementSpeed;
+
+            var newPosition = new Vector3(deltaX,deltaY,0);
+
+            _cont.Move( newPosition * Time.deltaTime );
+        }
+    }
 }
